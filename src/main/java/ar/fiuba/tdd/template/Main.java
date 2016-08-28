fe610694abf32f10cb0e406bbd084fa4efb6f9c3
juanmafc <juanmafc25@gmail.com>
2016-08-27 23:17:04 -0300

    Agrego sentencias para ver la salida en Travis

--- src/main/java/ar/fiuba/tdd/template/Main.java
+++ src/main/java/ar/fiuba/tdd/template/Main.java
@@ -3,5 +3,6 @@ package ar.fiuba.tdd.template;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is just a template project");
+        int i = 0; i++; i++; i++;
     }
 }
